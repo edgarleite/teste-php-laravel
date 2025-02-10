4bbfe233c1c3965f2f1a117cf8ab40a3591f6dc8
Edgar <edgarleitedacosta@gmasil.com>
2025-02-10 17:29:05 -0300

    Criando rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocumentImportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/import', [DocumentImportController::class, 'showForm'])->name('document.import.form');
+Route::post('/import', [DocumentImportController::class, 'import'])->name('document.import');
+Route::post('/process-queue', [DocumentImportController::class, 'processQueue'])->name('queue.process');
