c2b97615fbef700dbf2aa2291442cd471af9a3c3
Edgar <edgarleitedacosta@gmasil.com>
2025-02-10 17:35:08 -0300

    Corrigido campo no migration.

--- database/migrations/2023_03_28_172401_create_documents_table.php
+++ database/migrations/2023_03_28_172401_create_documents_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained()->onDelete('cascade'); // Esse método já cria o campo e a chave FK de relacionamento.
             $table->string('title', 150)->index(); // Prefiro definir um tamanho maior, afim de evitar truncar no insert. Indexar para melhoria de performance em busca.
-            $table->string('contents');
+            $table->text('contents');
             $table->timestamps(); // Prefiro deixar os timestamp no fim da tabela.
         });
     }
